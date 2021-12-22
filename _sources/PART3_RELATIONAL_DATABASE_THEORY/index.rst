7fc12a552c66ee6a1079c4ccb6f71cddbb2528a1
Christopher Painter-Wakefield <cpainter@mines.edu>
2021-12-22 15:30:39 -0700

    Eliminated some warning messages.

--- _sources/PART3_RELATIONAL_DATABASE_THEORY/index.rst
+++ _sources/PART3_RELATIONAL_DATABASE_THEORY/index.rst
@@ -9,9 +9,8 @@ RELATIONAL DATABASE THEORY
 
     01-relational-model/relational-model
     02-relational-algebra/relational-algebra
-    03-relational-calculus/relational-calculus
-    04-normalization/normalization
-    05-sql-vs-theory/sql-vs-theory
+    03-normalization/normalization
+    04-sql-vs-theory/sql-vs-theory
 
 
 |license-notice|
--- conf.py
+++ conf.py
@@ -79,7 +79,11 @@ release = '0.0'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = []
+exclude_patterns = [
+    '**/*.tex',
+    '**/Makefile',
+    '_static/theme-overrides.css'
+]
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
